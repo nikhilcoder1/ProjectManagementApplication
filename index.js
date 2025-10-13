c5582aad81fc3b5e22376a5a1a6867b6c8219e1e
dev_nick <developernikhil28@gmail.com>
2025-10-14 00:20:54 +0530

    Add dot env files on backend

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env",
+});
+
+let myusername = process.env.MY_APP_USERNAME; 
+
+console.log("Username from .env file:", myusername);
+
 console.log("Hello, World!");
 console.log("This is a sample Node.js application.");
-console.log("Enjoy coding!");
+console.log("Enjoy coding! 😊");
