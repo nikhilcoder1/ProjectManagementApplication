9315db5f7fb473648078471b69ed80aad68070ef
dev_nick <developernikhil28@gmail.com>
2025-10-14 18:19:54 +0530

    Few updates

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,15 +1,24 @@
 import { ApiResponse } from "../utils/api-response.js";
+import asyncHandler from "../utils/async-handler.js";
 
-const healthcheck = (req, res) => {
-    try {
-        res
-        .status(200)
-        .json(new ApiResponse(200, {message : "Service is up and running"}));
-    } catch (error) {
-        res
-        .status(500)
-        .json(new ApiResponse(500, "Internal Server Error", error.message));
-    }
-};
+/**
+    const healthcheck = async (req, res, next) => {
+        try {
+            const user = await getUserFromDb(); // Example async operation
+
+            res
+            .status(200)
+            .json(new ApiResponse(200, {message : "Service is up and running"}));
+        } catch (error) {
+            next(err);
+        }
+    };
+**/
+
+const healthcheck = asyncHandler(async (req, res) => {
+    res
+    .status(200)
+    .json(new ApiResponse(200, {message : "Service is up and running"}));
+});
 
 export { healthcheck };
