7f184963c3f2981546ab1a3fe71c56e169f5d4ef
dev_nick <developernikhil28@gmail.com>
2025-10-15 23:51:11 +0530

    Add usermodels

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose , { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema(
     {
@@ -75,5 +76,17 @@ const userSchema = new Schema(
     }
 );
 
+userSchema.pre("save", async function (next) 
+{
+    if (!this.isModified("password"))   
+    {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+    }).catch((err) => {
+        next(err);
+    }   );
 
 export const User = mongoose.model("User", userSchema);
