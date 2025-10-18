3189b0557fea6238b8fc35ffda72b993d18578bd
dev_nick <developernikhil28@gmail.com>
2025-10-19 01:42:05 +0530

    authRouter route added

--- src/app.js
+++ src/app.js
@@ -22,6 +22,9 @@ app.use(
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 app.use("/api/v1/healthcheck", healthcheckRouter);
 
+import authRouter from "./routes/auth.routes.js";
+app.use("/api/v1/auth", authRouter);
+
 app.get('/',(req, res)=>{
     res.send("Hello World!")
 })
