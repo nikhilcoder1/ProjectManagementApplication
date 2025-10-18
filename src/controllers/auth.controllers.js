0b504e6c4171b81fffeb24ead43ddefe1a60016f
dev_nick <developernikhil28@gmail.com>
2025-10-19 01:40:56 +0530

    File name change in import statement

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,7 +1,7 @@
-import { User } from "../models/user.model.js";
-import { ApiResponse } from "../utils/apiResponse.js";
-import { ApiError } from "../utils/apiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.models.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-errors.js";
+import asyncHandler from "../utils/async-handler.js";
 import { emailVerificationMailgenContent, sendEmail} from "../utils/mails.js";
 
 const generateAccessAndRefreshTokens = async (userId) => {
