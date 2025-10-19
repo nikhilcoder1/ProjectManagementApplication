d45d1e48c07019549a2a1360a14a2ebf5715f0b1
dev_nick <developernikhil28@gmail.com>
2025-10-19 14:01:47 +0530

    Some validation fixes

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,31 +1,47 @@
 import { body } from 'express-validator';
 
 const userRegisterValidator = () => {
-    return [
-        body('email')
-            .trim()
-            .isEmpty().withMessage('Email is required')
-            .isEmail().withMessage('Invalid email address'),
-        body('username')
-            .trim()
-            .isEmpty().withMessage('Username is required')
-            .isLowercase().withMessage('Username must be in lowercase')
-            .isString().withMessage('Username must be a string')
-            .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
-        body('password')
-            .trim()
-            .isEmpty().withMessage('Password is required')
-            .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-        body('fullName')
-            .optional()
-            .trim()
-            .isEmpty().withMessage('Full name is required')
-            .isString().withMessage('Full name must be a string')
-            .isLength({ min: 3 }).withMessage('Full name must be at least 3 characters long')
-    ];
-}
+  return [
+    body('email')
+      .trim()
+      .notEmpty().withMessage('Email is required')
+      .isEmail().withMessage('Invalid email address'),
+
+    body('username')
+      .trim()
+      .notEmpty().withMessage('Username is required')
+      .isLowercase().withMessage('Username must be in lowercase')
+      .isString().withMessage('Username must be a string')
+      .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+
+    body('password')
+      .trim()
+      .notEmpty().withMessage('Password is required')
+      .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+
+    body('fullName')
+      .optional()
+      .trim()
+      .isString().withMessage('Full name must be a string')
+      .isLength({ min: 3 }).withMessage('Full name must be at least 3 characters long')
+  ];
+};
+
+const userLoginValidator = () => {
+  return [
+    body('email')
+      .trim()
+      .notEmpty().withMessage('Email is required')
+      .isEmail().withMessage('Invalid email address'),
+
+    body('password')
+      .trim()
+      .notEmpty().withMessage('Password is required')
+  ];
+};
 
 
 export {
-    userRegisterValidator
+    userRegisterValidator ,
+    userLoginValidator
 };
