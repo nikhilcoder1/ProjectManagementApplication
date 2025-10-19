e495d0c103f1a91b67388399d828d1b2e66db3f1
dev_nick <developernikhil28@gmail.com>
2025-10-19 12:26:22 +0530

    Few updates

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -76,4 +76,48 @@ const registerUser = asyncHandler(async (req, res) => {
     );
 }); 
 
-export { registerUser };
+const login = asyncHandler(async (req, res) => {
+    const {email,password,username} = req.body
+
+    if(!email){
+        throw new ApiError(400, "Email is required");
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new ApiError(400, "User does not exists");
+    }
+
+    const isPasswordValid = user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(400, "Invalid credentials");
+    }
+    
+    const {accessToken , refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user.id).select("-password -refreshToken -refreshToken -emailVerificationToken -emailVerificationExpiry");
+
+    const cookieOptions = {
+        httpOnly: true,
+        secure : true,
+    };
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, cookieOptions)
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .json(
+            new ApiResponse(
+                200,
+                {user: loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User logged in successfully"
+            )
+        );
+});
+
+export { registerUser , login };
