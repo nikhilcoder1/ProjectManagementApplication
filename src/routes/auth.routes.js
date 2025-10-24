749b423a28bac4d7beceb650b7be9659b1dd6ea2
dev_nick <developernikhil28@gmail.com>
2025-10-24 23:58:04 +0530

    Added unsecured routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,16 +1,25 @@
 import { Router } from "express";
-import { login, logoutUser, registerUser } from "../controllers/auth.controllers.js";
+import { login, logoutUser, refreshAccessToken, registerUser, verifyEmail } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { userLoginValidator, userRegisterValidator } from "../validators/index.js";
+import { userForgotPasswordValidator, userLoginValidator, userRegisterValidator, userResetForgotPasswordValidator } from "../validators/index.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// Public routes(unsecured)
 router.route("/register").post(userRegisterValidator() , validate , registerUser);
 
 router.route("/login").post(userLoginValidator() , validate , login);
 
-// Protected route
+router.route("/verify-email/:verficationToken").get(verifyEmail);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/forgot-password").post(userForgotPasswordValidator() , validate , forgotPaasswordRequest);
+
+router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator() , validate , resetForgotPassword);
+
+// Secured route
 router.route("/logout").post(verifyJWT , logoutUser);
 
 export default router;
